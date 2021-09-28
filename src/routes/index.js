f382239da816abbb8d9cd65f23f182ccfd9c161c
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-28 16:36:35 -0300

    styling DrawerScreen Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import Movies from '../pages/Movies';
 
 import StackRoutes from './StackRoutes';
@@ -23,7 +25,20 @@ function Routes() {
         drawerInactiveTintColor: '#ffffff',
       }}
     >
-      <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
+      <Drawer.Screen
+        name="HomeDrawer"
+        component={StackRoutes}
+        options={{
+          title: 'Home',
+          drawerIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'movie-open' : 'movie-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
 
       <Drawer.Screen name="Movies" component={Movies} />
     </Drawer.Navigator>

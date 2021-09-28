881350c11cbdf3ece973ce0c66d9d0321513d264
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-28 16:31:25 -0300

    styling DrawerNavigator

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,20 @@ const Drawer = createDrawerNavigator();
 
 function Routes() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+
+        drawerStyle: {
+          backgroundColor: '#090a0e',
+          paddingTop: 20,
+        },
+
+        drawerActiveBackgroundColor: '#e72f49',
+        drawerActiveTintColor: '#ffffff',
+        drawerInactiveTintColor: '#ffffff',
+      }}
+    >
       <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
 
       <Drawer.Screen name="Movies" component={Movies} />

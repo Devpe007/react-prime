643f775e4e00c17f8a17841bd98a2aa3428a4612
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-01 20:14:08 -0300

    add stars rate in Detail page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Feather, Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
+import { Feather, Ionicons } from '@expo/vector-icons';
+import Stars from 'react-native-stars';
 
 import {
   Container,
@@ -9,6 +11,8 @@ import {
   Banner,
   ButtonLink,
   Title,
+  ContentArea,
+  Rate,
 } from './styles';
 
 import api, { key } from '../../services/api';
@@ -80,6 +84,38 @@ function Detail() {
       </ButtonLink>
 
       <Title numberOfLine={2}>{movie.title}</Title>
+
+      <ContentArea>
+        <Stars
+          default={movie.vote_average}
+          count={10}
+          half={true}
+          starSize={20}
+          fullStar={(
+            <Ionicons
+              name="md-star"
+              size={24}
+              color="#e7a74e"
+            />
+          )}
+          emptyStar={(
+            <Ionicons
+              name="md-star-outline"
+              size={24}
+              color="#e7a74e"
+            />
+          )}
+          halfStar={(
+            <Ionicons
+              name="md-star-half"
+              size={24}
+              color="#e7a74e"
+            />
+          )}
+          disable={true}
+        />
+        <Rate>{movie.vote_average}</Rate>
+      </ContentArea>
     </Container>
   );
 };

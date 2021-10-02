068ffca05ff096b602232cc8f72e5fbf750d7cfc
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-01 21:34:13 -0300

    create ModalLink component

---
+++ src/components/ModalLink/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+import { Feather } from '@expo/vector-icons';
+
+import { BackButton, Name } from './styles';
+
+function ModalLink({ link, title, closeModal }) {
+  return (
+    <>
+      <BackButton onPress={closeModal}>
+        <Feather name="x" size={35} color="#ffffff" />
+        <Name numberOfLines={1}>{title}</Name>
+      </BackButton>
+
+      <WebView
+        source={{ uri: link }}
+      />
+    </>
+  );
+};
+
+export default ModalLink;
